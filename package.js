84b49151e5e4dea910acba926851ec96f71faf59
WENS-V4 <bywensoffby43@gmail.com>
2026-04-01 03:24:04 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "WENS-DM-BOT",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "baileys": "npm:baileys@latest",
+    "axios": "^1.7.7",
+    "pino": "^8.18.0",
+    "express": "^4.18.2"
+  }
+}
